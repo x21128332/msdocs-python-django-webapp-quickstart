ef318950fb683ceb8ec45fb52786e30a0cf009e3
Aisling Kearney <91568562+x21128332@users.noreply.github.com>
2023-03-10 14:10:57 +0000

    Update settings.py

--- quickstartproject/settings.py
+++ quickstartproject/settings.py
@@ -117,10 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
+#STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
 STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://msdocs-python-webapp-quickstart-103.azurewebsites.net/']
